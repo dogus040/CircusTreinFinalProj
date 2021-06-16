092d11dee7f96427914f10c2e3c17685dd6dcef1
Özdemir,Dogus D <470811@student.fontys.nl>
2021-06-16 06:24:47 +0200

    add IntegrationTest project to CircusTrein

---
+++ CircusTreinFinalProj/CircusTreinFinalProj_IntegTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CircusTreinFinalProj_IntegTests
+{
+    [TestClass]
+    public class Train_To_Wagon_Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
